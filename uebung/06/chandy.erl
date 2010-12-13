836bdcf4e3497fb7abb49b13c0965d011a9d92cc
Dominik Oepen <oepen@informatik.hu-berlin.de>
2010-12-14 00:42:41 +0100

    Receive-loop in p2 eingebaut

--- uebung/06/chandy.erl
+++ uebung/06/chandy.erl
@@ -4,13 +4,19 @@
 p1({Schrauben, Euro}, P2_PID) ->
     io:format("P1 meldet sich zum Dienst: P1_PID=~w P2_PID=~w~n",[self(),P2_PID]),
     io:format("P1 hat ~w Schrauben und ~w Euro~n", [Schrauben, Euro]),
-    P2_PID ! {pid, self()}.
+    P2_PID ! {pid, self()},
+    P2_PID ! chandy.
 
 p2({Schrauben, Euro}) ->
     io:format("P2 meldet sich zum Dienst: P2_PID=~w~n", [self()]),
     receive
         {pid, P1_PID} ->
-            io:format("P2 empfaengt: P1_PID=~w~n", [P1_PID])
+            io:format("P2 empfaengt: P1_PID=~w~n", [P1_PID]),
+            p2({Schrauben, Euro});
+        %Chandy-Lamport: First we dump our state
+        chandy ->
+            io:format("State of ~w: ~w Schrauben und ~w Euro ~n",[self(), Schrauben, Euro]),
+            p2({Schrauben, Euro})
     end.
 
 start() ->
