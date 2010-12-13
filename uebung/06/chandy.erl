0240c76a7fc4c9a32c7e3fe039318bc866d9da7b
Dominik Oepen <oepen@informatik.hu-berlin.de>
2010-12-14 00:24:40 +0100

    Prozesse haben nun einen Zustand

--- uebung/06/chandy.erl
+++ uebung/06/chandy.erl
@@ -1,17 +1,18 @@
 -module(chandy).
--export([start/0,p1/1,p2/0]).
+-export([start/0,p1/2,p2/1]).
 
-p1(P2_PID) ->
+p1({Schrauben, Euro}, P2_PID) ->
     io:format("P1 meldet sich zum Dienst: P1_PID=~w P2_PID=~w~n",[self(),P2_PID]),
+    io:format("P1 hat ~w Schrauben und ~w Euro~n", [Schrauben, Euro]),
     P2_PID ! {pid, self()}.
 
-p2() ->
+p2({Schrauben, Euro}) ->
     io:format("P2 meldet sich zum Dienst: P2_PID=~w~n", [self()]),
     receive
         {pid, P1_PID} ->
-            io:format("P2 empfängt: P1_PID=~w~n", [P1_PID])
+            io:format("P2 empfaengt: P1_PID=~w~n", [P1_PID])
     end.
 
 start() ->
-    P2_PID = spawn(chandy, p2, []),
-    spawn(chandy, p1, [P2_PID]).
+    P2_PID = spawn(chandy, p2, [{2000,50}]),
+    spawn(chandy, p1, [{0,900},P2_PID]).
